becf9be678dbcb1696b96da37768f6804fdb6ac5
Liuming9124 <a723479.a12@gmail.com>
2025-01-08 00:56:52 +0800

    server connect to fastapi

--- main/route/modules/uiapp.js
+++ main/route/modules/uiapp.js
@@ -12,9 +12,29 @@ router.post('/vtocmsg', async (req, res) => {
     data = req.body;
     console.log(data);
     msg = data[0].msg;
-    returnmsg = "receive " + msg;
-    response = [{"msg":returnmsg}];
-    res.send(response);
+    input = {
+        "detail": [
+            {
+                "input": {
+                    "msg": msg
+                }
+            }
+        ]
+    }
+
+    // 發 post request 
+    await fetch('http://localhost:8000/asklm', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(input),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log('Success:', data);
+        res.send(data);
+    })
 })
 
 
