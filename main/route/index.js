55f392c4196cf8a2adab55d7a7d27ef23a437974
Liuming9124 <a723479.a12@gmail.com>
2025-01-08 00:57:55 +0800

    modify

--- main/route/index.js
+++ main/route/index.js
@@ -1,13 +1,12 @@
 const ctrl = require('./modules/ctrl.js')
 const uiapp = require('./modules/uiapp')
-const llm   = require('./modules/llm')
 const dbctl = require('./modules/dbctl')
 
 module.exports = app => {
   app.use('/ctrl',  ctrl)
   app.use('/uiapp',  uiapp)
-  app.use('/llm',    llm)
   app.use('/dbctl',  dbctl)
+  
   app.post('/vtocvoice', async (req, res) => {
     console.log('vtocvoice in')
     res.send("vtocvoice");
