ad7b0dc062990a2fa3d5fc157b8cc5fb035edfb1
Liuming9124 <a723479.a12@gmail.com>
2025-01-08 17:43:28 +0800

    finish backend whisper

--- main/route/modules/uiapp.js
+++ main/route/modules/uiapp.js
@@ -46,12 +46,57 @@ router.post('/vtocmsg', async (req, res) => {
 
 
 router.post('/vtocvoice', async (req, res) => {
-    data = req.body;
-    console.log(data);
-    msg = data[0].msg;
-    returnmsg = "receive " + msg;
-    response = [{"msg":returnmsg}];
-    res.send(response);
+    const data = req.body;
+
+    const msg = data[0].msg;
+    const input = {
+        detail: [
+            {
+                input: {
+                    msg: msg
+                }
+            }
+        ]
+    };
+
+    const fastapi = config.fastapi;
+
+    try {
+        // 發送第一個 POST 請求到 /asklm
+        const asklmResponse = await fetch(fastapi + '/translate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(input),
+        });
+
+        const asklmData = await asklmResponse.json();
+        console.log('Response from /translate:', asklmData.msg);
+
+        // 發送第二個 POST 請求到 /translate，使用 /asklm 的回應
+        const vtocmsgInput = [{msg: asklmData.msg}];
+        console.log(config.http + '/uiapp/vtocmsg');
+        const vtocmsgResponse = await fetch( config.http + 'uiapp/vtocmsg', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(vtocmsgInput),
+        });
+
+        const vtocmsgData = await vtocmsgResponse.text();
+        console.log('Response from /vtocmsg:', vtocmsgData);
+
+        const result = [
+            { input: asklmData },
+            { output: vtocmsgData }
+        ];
+        res.send(result);
+    } catch (error) {
+        console.error('Error in /vtocvoice:', error);
+        res.status(500).send({ error: 'Failed to process vtocvoice' });
+    }
 })
 
 
